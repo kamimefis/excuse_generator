a1823638ab64d9817006479ed26a34b9312a0538
Camila <camilalobos.2307@gmail.com>
2020-09-06 23:39:06 -0300

    random generator

---
+++ app.js
@@ -0,0 +1,16 @@
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+
+//const randomElement = array[Math.floor(Math.random() * array.length)];
+
+let randomWho= who[Math.floor(Math.random()*who.length)];
+let randomAction= action[Math.floor(Math.random()*action.length)];
+let randomWhat= what[Math.floor(Math.random()*what.length)];
+let randomWhen= when[Math.floor(Math.random()*when.length)];
+
+let phrase= (randomWho + ' '+ randomAction+ ' '+ randomWhat+ ' '+ randomWhen);
+
+document.querySelector('#excuse').innerHTML= phrase; 
